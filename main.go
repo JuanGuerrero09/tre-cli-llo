969bcafcd053a9d364fcdd922ac7499a26abf04f
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2024-01-31 17:36:02 +0100

    itel delegate struct added

--- main.go
+++ main.go
@@ -1,43 +1,163 @@
+// type status int
+
+// const (
+// 	todo status = iota
+// 	inProgress
+// 	done
+// )
+
+// type Task struct {
+// 	title string
+// 	description string
+// 	Status status
+// }
+
+// func (t *Task) FilterValue() string {
+// 	return t.title
+// }
+
+// func (t *Task) Title() string {
+// 	return t.title
+// }
+
+// func (t *Task) Description() string {
+// 	return t.description
+// }
+
+// type Model struct {
+// 	list list.Model
+// 	err error
+// }
+
+// func (m *Model) initList() {
+// 	m.list = list.New([]list.Item{}, list.NewDefaultDelegate())
+// }
+
 package main
 
 import (
-	"github.com/charmbracelet/bubbles"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type status int
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
-const (
-	todo status = iota
-	inProgress
-	done
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
 )
 
-
-type Task struct {
-	title string
-	description string
-	Status status
+type item struct {
+	title     string
+	desc      string
+	extra     string
+	startDate time.Time
+	dueDate   time.Time
 }
 
-func (t *Task) FilterValue() string {
-	return t.title
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) Extra() string       { return i.extra }
+func (i item) StartDate() string   { return i.startDate.String() }
+func (i item) DueDate() string     { return i.dueDate.String() }
+func (i item) FilterValue() string { return i.title }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
 }
 
-func (t *Task) Title() string {
-	return t.title
-}
-
-func (t *Task) Description() string {
-	return t.description
-}
-
-type Model struct {
+type model struct {
 	list list.Model
-	err error
 }
 
-func (m *Model) initList() {
-	m.list = list.New([]list.Item{}, list.NewDefaultDelegate())
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
+}
+
+func main() {
+	items := []list.Item{
+		item{title: "Raspberry Pi’s", extra: "I have ’em all over my house!", startDate: time.Now()},
+		item{title: "Nutella", desc: "It's good on toast"},
+		item{title: "Bitter melon", desc: "It cools you down"},
+		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
+		item{title: "Eight hours of sleep", desc: "I had this once"},
+		item{title: "Cats", desc: "Usually"},
+		item{title: "Plantasia, the album", desc: "My plants love it too"},
+		item{title: "Pour over coffee", desc: "It takes forever to make though"},
+		item{title: "VR", desc: "Virtual reality...what is there to say?"},
+		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
+		item{title: "Linux", desc: "Pretty much the best OS"},
+		item{title: "Business school", desc: "Just kidding"},
+		item{title: "Pottery", desc: "Wet clay is a great feeling"},
+		item{title: "Shampoo", desc: "Nothing like clean hair"},
+		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
+		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
+		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
+		item{title: "Stickers", desc: "The thicker the vinyl the better"},
+		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
+		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
+		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
+		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
+		item{title: "Terrycloth", desc: "In other words, towel fabric"},
+	}
+
+	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "My Fave Things"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
